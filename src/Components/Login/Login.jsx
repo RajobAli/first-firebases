ea18a6b0c78ec99a5b37e527c0b66f0b32dd2329
RajobAli <freelancerrajobali@gmail.com>
2023-10-02 13:33:11 +0600

    login and logout with github

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,57 +1,76 @@
 
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 import app from "../../Firebase/Firebase.config";
 import { useState } from "react";
 
 const Login = () => {
-    const [user,setUser] = useState()
-  const auth = getAuth(app);
+    const [user, setUser] = useState()
+    const auth = getAuth(app);
 
-  const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
-  const handleGoogleSignIn = () =>{
-    signInWithPopup(auth,provider)
-    .then(result =>{
-        const LogedInUser = result.user;
-        console.log(LogedInUser);
-        setUser(LogedInUser)
-        
-    })
-    .catch(error=>{
-        console.log(error.message)
-    })
-   
-  }
-const handleSignOut = () =>{
-    signOut(auth)
-    .then(result =>{
-        console.log(result)
-        setUser(null)
-    })
-    .catch(error =>{
-        console.log(error.message)
-    })
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const LogedInUser = result.user;
+                console.log(LogedInUser);
+                setUser(LogedInUser)
 
-}
-  
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+    }
+    const handleGitHubSignIn = () =>{
+        signInWithPopup(auth,githubProvider)
+        .then(result =>{
+            const LogedInUser = result.user;
+            console.log(LogedInUser)
+            setUser(LogedInUser)
+        })
+        .catch(error =>{
+            console.log(error.message)
+        })
+    }
+
     return (
         // user ? logout : sign in
 
         <div>
-   { user ? <button onClick={handleSignOut}>Sign Out</button> :
-     <button onClick={handleGoogleSignIn}> Google Login</button>}
-    {user &&
-         <div>
-         <h3>User:{user.displayName}</h3>
-         <h3>Email:{user.email}</h3>
-         <img src= {user.photoURL} alt=""></img>
-         <h3>User Id:{user.uid}</h3>
-  
-         </div>
-    
-    }
-            
+            {user ?
+                <button onClick={handleSignOut}>Sign Out</button> :
+                <>
+                    <button onClick={handleGoogleSignIn}> Google Login</button>
+                    <button onClick={handleGitHubSignIn}>Github Login</button>
+                </>
+
+
+            }
+            {user &&
+                <div>
+                    <h3>User:{user.displayName}</h3>
+                    <h3>Email:{user.email}</h3>
+                    <img src={user.photoURL} alt=""></img>
+                    <h3>User Id:{user.uid}</h3>
+
+                </div>
+
+            }
+
         </div>
     );
 };
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 
+// Local Link:https://github.com/RajobAli/first-firebases
+// Live Link : https://651a61c6e86c39521f8245ea--classy-faloodeh-c65dc6.netlify.app/
+
 
 const Main = () => {
     return (
